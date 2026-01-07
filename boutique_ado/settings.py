5c782993c6b09912ca216d08cc953559184c8117
Adrian <h7qju7ts@students.codeinstitute.net>
2026-01-07 10:42:42 +0000

    Tested stripe hooks

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.01',#vscode preview
+    'localhost',#listen for stripe webhooks
+]
 
 
 # Application definition
@@ -181,6 +184,7 @@ if os.path.isfile('env.py'):
 
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ checkout/webhook_handler.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+
+class StripeWH_Handler:
+    """
+    Handle Stripe webhooks
+    """
+    def __init__(self, request):
+        self.request = request
+    
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandeled webhook recived: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook recived: {event["type"]}',
+            status=200)
+    
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook recived: {event["type"]}',
+            status=200)
+    

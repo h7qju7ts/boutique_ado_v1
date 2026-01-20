5326f7c174d6b5c8fbedd8c4da653523b272a8bb
Adrian <h7qju7ts@students.codeinstitute.net>
2026-01-20 19:47:35 +0000

    typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -189,7 +189,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'Europe (Ireland) eu-west-1' # change this to your bucket region
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{'myschool-bucket-codeinstitute'}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
     # Static and media files

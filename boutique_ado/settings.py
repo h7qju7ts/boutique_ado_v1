2aadfdccf49bf2696f84771f2ace6fe73f5ae6fe
Adrian <h7qju7ts@students.codeinstitute.net>
2026-01-22 09:54:33 +0000

    change aws region name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -188,7 +188,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'myschool-bucket-codeinstitute' # change this to your AWS bucket name
-    AWS_S3_REGION_NAME = 'Europe (Ireland) eu-west-1' # change this to your bucket region
+    AWS_S3_REGION_NAME = 'eu-west-1' # change this to your bucket region
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'

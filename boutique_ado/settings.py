79d9427485bb5661287312801dca4788fbd77d9c
Adrian <h7qju7ts@students.codeinstitute.net>
2026-01-23 10:46:24 +0000

    uptate env.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -231,6 +231,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
